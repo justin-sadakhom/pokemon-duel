37312c0a3ca067957c9338b9d3177333e4cefe26
justinsadakhom <justin.sadakhom@gmail.com>
2020-07-22 12:34:39 -0400

    Add sample pokemon species data file + finish up Pokemon class framework

--- src/main/java/elitefour/pokemonduel/Pokemon.java
+++ src/main/java/elitefour/pokemonduel/Pokemon.java
@@ -1,34 +1,152 @@
 package elitefour.pokemonduel;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Pokemon {
+    
+    public enum Stat {
+        ATTACK,
+        DEFENSE,
+        SPECIAL_ATTACK,
+        SPECIAL_DEFENSE,
+        SPEED,
+        ACCURACY,
+        EVASION
+    }
     
     private final String name;
     private final Type[] type;
     private final Move[] moves;
     private final Nature nature;
-    
     private Status status;
     
     /* Statistics fields */
     
-    private final int[] baseStats;
-    private int[] statStages;
+    private final int[] baseStats, ivs, evs;
+    private Map<Stat, Integer> statStages;
     private int[] stats;
     
-    public Pokemon(String species, Nature nature) {
+    /* Constructor with default values for fields. */
+    
+    public Pokemon(String species) {
         
-        name = species;
-        type = new Type[2];
-        moves = new Move[4];
+        this.name = species;
+        
+        Type[] tempType = new Type[2];
+        
+        try {
+            tempType = initType();
+        } catch (Exception error) {
+            error.printStackTrace(System.out);
+        }
+        
+        this.type = tempType;
+        this.moves = new Move[4];
+        this.nature = Nature.Hardy;
+        this.status = new Status();
+        
+        int[] tempBaseStats = new int[6];
+        
+        try {
+            tempBaseStats = initBaseStats();
+        } catch (Exception error) {
+            error.printStackTrace(System.out);
+        }
+        
+        this.baseStats = tempBaseStats;
+        this.ivs = new int[6];
+        this.evs = new int[6];
+        this.statStages = Map.of(Stat.ATTACK, 0, Stat.DEFENSE, 0,
+                Stat.SPECIAL_ATTACK, 0, Stat.SPECIAL_DEFENSE, 0,
+                Stat.ACCURACY, 0, Stat.EVASION, 0);
+        this.stats = new int[8];
+    }
+    
+    /* Constructor that requires specific parameters. */
+    
+    public Pokemon(String species, Nature nature,
+            int[] ivs, int[] evs, Move[] moves) {
+        
+        this.name = species;
+        
+        Type[] tempType = new Type[2];
+        
+        try {
+            tempType = initType();
+        } catch (Exception error) {
+            error.printStackTrace(System.out);
+        }
+        
+        this.type = tempType;
+        this.moves = moves;
         this.nature = nature;
+        this.status = new Status();
         
-        status = new Status();
+        int[] tempBaseStats = new int[6];
         
-        baseStats = new int[6];
-        statStages = new int[7];
-        stats = new int[8];
+        try {
+            tempBaseStats = initBaseStats();
+        } catch (Exception error) {
+            error.printStackTrace(System.out);
+        }
+        
+        this.baseStats = tempBaseStats;
+        this.ivs = ivs;
+        this.evs = evs;
+        this.statStages = Map.of(Stat.ATTACK, 0, Stat.DEFENSE, 0,
+                Stat.SPECIAL_ATTACK, 0, Stat.SPECIAL_DEFENSE, 0,
+                Stat.ACCURACY, 0, Stat.EVASION, 0);
+        this.stats = new int[8];
+    }
+    
+    private Type[] initType() throws Exception {
+        
+        File file = new File("resources\\data\\pokemon_species.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        
+        String line = "";
+        String[] typing = new String[2];
+        
+        while (line != null && !line.equals(name))
+            line = reader.readLine();
+        
+        if (line.equals(name)) {
+            line = reader.readLine();
+            typing = line.split("/");
+        }
+            
+        return new Type[]{Type.valueOf(typing[0]), Type.valueOf(typing[1])};
+    }
+    
+    private int[] initBaseStats() throws Exception {
+        
+        File file = new File("resources\\data\\pokemon_species.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        
+        String line = "";
+        String[] species_stats = new String[2];
+        
+        while (line != null && !line.equals(name))
+            line = reader.readLine();
+        
+        if (line.equals(name)) {
+            
+            for (int i = 0; i < 2; i++)
+                line = reader.readLine();
+            
+            species_stats = line.split("/");
+        }
+        
+        return new int[]{Integer.parseInt(species_stats[0]),
+            Integer.parseInt(species_stats[1]),
+            Integer.parseInt(species_stats[2]),
+            Integer.parseInt(species_stats[3]),
+            Integer.parseInt(species_stats[4]),
+            Integer.parseInt(species_stats[5]),};
     }
     
     public String getName() {
