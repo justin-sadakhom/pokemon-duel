3d726e9a91d0ba2f22cdab67d3bc3196dde1375c
justinsadakhom <justin.sadakhom@gmail.com>
2020-08-06 17:49:28 -0400

    Write battle text for moves that apply status conditions

--- src/main/java/elitefour/pokemonduel/DamageStatus.java
+++ src/main/java/elitefour/pokemonduel/DamageStatus.java
@@ -31,10 +31,48 @@ public class DamageStatus extends DamageMove {
         return success;
     }
     
-    private boolean applyEffect(Pokemon target) {
+    protected boolean applyEffect(Pokemon target) {
         if (appliedStatus.mixStatus().isEmpty())
             return target.setStatus(appliedStatus.loneStatus());
         else
             return target.setStatus(appliedStatus.mixStatus().get(0));
+    }
+    
+    public static String hitText(Status.LoneStatus status, Pokemon target,
+            boolean success) {
+        
+        if (!success)
+            return "...but it failed.";
+        
+        switch (status) {
+            case BURN:
+                return target.name() + " was burned!";
+            case FREEZE:
+                return target.name() + " was frozen solid!";
+            case PARALYSIS:
+                return target.name() + " was paralyzed!";
+            case POISON:
+                return target.name() + " was poisoned!";
+            case TOXIC:
+                return target.name() + " was badly poisoned!";
+            case SLEEP:
+                return target.name() + " fell asleep!";
+            default: // This should never be reached.
+                return "";
+        }
+    }
+    
+    public static String hitText(Status.MixStatus status, Pokemon target,
+            boolean success) {
+        
+        if (!success)
+            return "...but it failed.";
+        
+        switch (status) {
+            case CONFUSION:
+                return target.name() + " became confused!";
+            default:
+                return "";
+        }
     }
 }
